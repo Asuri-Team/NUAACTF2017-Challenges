2bc943cab7b8ac54a1a15f62e38ca461de093cde
Yibai Zhang <xm1994@gmail.com>
2017-10-13 11:44:25 +0800

    Add Dockerfile and flag for pwn, modify pillow Dockerfile.

--- pwn/hello_pwn/hello_pwn.c
+++ pwn/hello_pwn/hello_pwn.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-char flag[] = "flag{th1s_Hel10_W0r1d}";  //bin for server
-// char flag[] = "flag{flag is on server}";   //bin for user
+#include <stdlib.h>
+#include <unistd.h>
 
 struct bof
 {
@@ -10,14 +9,21 @@ struct bof
 	
 }test;
 
+int hack(){
+	system("cat flag.txt");
+	return 0;
+}
+
 int main()
 {
+	alarm(60);
+	setbuf(stdout, 0);
 	puts("~~ welcome to nuaa ctf ~~");
 	puts("lets get helloworld for bof");
 	read(0,&test.input,16);
 	if (test.key==0x6e756161)
 	{
-		printf("%s\n", flag);
+		hack();
 	}
     return 0;
 }
